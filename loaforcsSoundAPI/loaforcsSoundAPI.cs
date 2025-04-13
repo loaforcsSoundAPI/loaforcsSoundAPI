6d41c513d0f95d79f062991660390721abb8d515
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2025-04-13 10:40:09 +1000

    add temp config for scene awake fix

--- loaforcsSoundAPI/loaforcsSoundAPI.cs
+++ loaforcsSoundAPI/loaforcsSoundAPI.cs
@@ -15,16 +15,17 @@ class loaforcsSoundAPI : BaseUnityPlugin {
 	internal new static ManualLogSource Logger { get; private set; }
 
 	static loaforcsSoundAPI _instance;
-    
+
 	void Awake() {
 		_instance = this;
 		Logger = BepInEx.Logging.Logger.CreateLogSource(MyPluginInfo.PLUGIN_GUID);
 		Config.SaveOnConfigSet = false;
-		
+
 		Logger.LogInfo("Setting up config");
 		Debuggers.Bind(Config);
 		SoundReportHandler.Bind(Config);
-        
+		SoundReplacementHandler.Bind(Config);
+
 		Logger.LogInfo("Running patches");
 		Harmony harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MyPluginInfo.PLUGIN_GUID);
 		UnityObjectPatch.Init(harmony);
@@ -33,14 +34,14 @@ class loaforcsSoundAPI : BaseUnityPlugin {
 		SoundAPI.RegisterAll(Assembly.GetExecutingAssembly());
 
 		SoundAPIAudioManager.SpawnManager();
-		
+
 		SoundReplacementHandler.Register();
 		Config.Save();
-		
+
 		Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} by loaforc has loaded :3");
 	}
 
 	internal static ConfigFile GenerateConfigFile(string name) {
-		return new ConfigFile(Utility.CombinePaths(Paths.ConfigPath, name + ".cfg"), saveOnInit: false, MetadataHelper.GetMetadata(_instance));
+		return new ConfigFile(Utility.CombinePaths(Paths.ConfigPath, name + ".cfg"), false, MetadataHelper.GetMetadata(_instance));
 	}
 }
